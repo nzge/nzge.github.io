4dac1720b87f16288bc6a603635396e0cee982f1
nzge <nzge@users.noreply.github.com>
2025-03-05 02:14:44 +0000

    deploy: a15aedf4d2e6f70120934c4e99ef708394099845

---
+++ assets/js/projects.js
@@ -0,0 +1,153 @@
+
+// Define project arrays for each webpage
+const classProjects = [
+  { 
+      title: 'Capstone Robot', 
+      date: new Date('2024-01-15'), 
+      image: '/assets/images/capstone_robot.png', 
+      description: 'Fully Autonomous delivery robot.' 
+  },
+  { 
+      title: 'Model Rocket', 
+      date: new Date('2023-03-10'), 
+      image: '/assets/images/model_rocket.png', 
+      description: 'G-class hobby rocket with a target apogee of 2,750 ft.' 
+  },
+  { 
+    title: 'Solar Powered Mini Car', 
+    date: new Date('2022-03-10'), 
+    image: '/assets/images/solar_car.PNG', 
+    description: 'A solar-powered mini car featuring a front-wheel drive, a bevel gear drivetrain, and topology optimizations.' 
+}
+];
+
+const racingProjects = [
+  { 
+      title: 'CV Boot Casting', 
+      date: new Date('2022-02-20'), 
+      image: '/assets/images/CV_boot.PNG',  
+      description: '3D-printed ribbed and ribless CV boot mold designs' 
+  },
+  { 
+      title: 'Tire Temperature Sensor Mount', 
+      date: new Date('2022-04-05'), 
+      image: '/assets/images/temp_sensor.PNG', 
+      description: 'Wheel temperature sensor mounting solution' 
+  },
+  { 
+      title: 'Aerofoil', 
+      date: new Date('2022-04-05'), 
+      image: '/assets/images/placeholder.jpg', 
+      description: 'Aero-package R&D via computational methods' 
+  },
+  { 
+    title: 'Laptime Simulation', 
+    date: new Date('2022-04-05'), 
+    image: '/assets/images/placeholder.jpg', 
+    description: 'Quarter car suspension kinematics model using Simulink and Simscape' 
+  }
+];
+
+const personalProjects = [
+  { 
+      title: 'Macropad', 
+      date: new Date('2022-02-20'), 
+      image: '/assets/images/macropad.png',  
+      description: 'An investigation into the customization potential of input devices and their core functionality.' 
+  },
+  { 
+      title: 'Kelvin Helmholtz Instability', 
+      date: new Date('2022-04-05'), 
+      image: '/assets/images/placeholder.jpg', 
+      description: 'Kelvin–Helmholtz instability with a discontinuous Galerkin spectral element method on a hierarchical Cartesian mesh with ADR' 
+  }
+];
+
+// Function to display projects based on current webpage
+function displayProjects() {
+  let projectContainer = document.getElementById('projectContainer');
+  projectContainer.innerHTML = ''; // Clear previous content
+  let projects = [];
+
+  // Determine which project array to use based on the current webpage
+  if (document.title === 'Nathan Ge - class projects') {
+      projects = classProjects;
+  } else if (document.title === 'Nathan Ge - racing projects') {
+      projects = racingProjects;
+  }
+  else if (document.title === 'Nathan Ge - personal projects') {
+    projects = personalProjects;
+  }
+
+  // Generate HTML content for each project and append it to the project container
+  projects.forEach(project => {
+      let projectElement = document.createElement('div');
+      projectElement.innerHTML = `
+          <a href="${generateProjectLink(project)}"><h2>${project.title}</h2></a>
+          <img src="${project.image}" alt="${project.title}">
+          <p>${project.description}</p>
+      `;
+      projectContainer.appendChild(projectElement);
+  });
+}
+
+function generateProjectLink(project) {
+    let projectIdentifier = project.title.replace(/\s+/g, '-').toLowerCase();
+    // Append a unique identifier to the URL
+    return `../projects/project_details/${projectIdentifier}.html`;
+}
+
+
+// Function to sort projects
+function sortProjects() {
+  const sortCriteria = document.getElementById('sortDropdown').value;
+  let projects = [];
+
+  // Determine which project array to use based on the current webpage
+  if (document.title === 'Nathan Ge - class projects') {
+      projects = classProjects;
+  } else if (document.title === 'Nathan Ge - racing projects') {
+      projects = racingProjects;
+  }
+  else if (document.title === 'Nathan Ge - personal projects') {
+    projects = personalProjects;
+  }
+
+  if (sortCriteria === 'date') {
+      projects.sort((a, b) => b.date - a.date); // Sort by date
+  } else if (sortCriteria === 'name') {
+      projects.sort((a, b) => a.title.localeCompare(b.title)); // Sort by name
+  }
+
+  displayProjects(); // Call displayProjects() without passing any arguments
+}
+
+// Call the displayProjects function when the page is loaded
+window.onload = displayProjects;
+
+
+
+// // Function to display projects
+// function displayProjects(projects) {
+//   const projectContainer = document.getElementById('projectContainer');
+//   projectContainer.innerHTML = ''; // Clear previous content
+  
+//   projects.forEach(project => {
+//       const projectElement = document.createElement('div');
+//       projectElement.classList.add('project');
+
+//       const titleElement = document.createElement('h2');
+//       titleElement.textContent = project.title;
+//       projectElement.appendChild(titleElement);
+
+//       const imageElement = document.createElement('img');
+//       imageElement.src = project.image;
+//       projectElement.appendChild(imageElement);
+
+//       const descriptionElement = document.createElement('p');
+//       descriptionElement.textContent = project.description;
+//       projectElement.appendChild(descriptionElement);
+
+//       projectContainer.appendChild(projectElement);
+//   });
+// }
