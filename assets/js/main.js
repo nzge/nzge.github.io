7369a6e8ff371db0da539dc7e23ab7b3e6840936
nzge <nzge@users.noreply.github.com>
2025-04-22 20:32:47 +0000

    deploy: 0db89a5410874cb407e47c1aa2a2d7b9d5fdb2c8

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,16 @@ jQuery(window).on('load', function(){
   new WOW().init(); 
 });
 
+
+
+
 // JavaScript function to go back to the previous page
 function goBack() {
     window.history.back();
 }
+
+
+
 
 // Function to dynamically sort the projects based on the selected option
 function sortProjects() {
@@ -53,4 +59,37 @@ function sortProjects() {
   sortedProjects.forEach(project => {
     projectsContainer.appendChild(project);
   });
+}
+
+
+
+
+// Slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
