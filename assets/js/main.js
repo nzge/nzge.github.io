713bfd355cb91bd3475bba76d80b462c631f749f
nzge <nzge@users.noreply.github.com>
2025-05-06 05:06:22 +0000

    deploy: dccc7d5ddd25179b9849c8fb9f763317eabf9505

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,33 +98,3 @@ document.addEventListener('DOMContentLoaded', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   });
 });
-
-///////////////////////////////
-
-// gifs
-document.addEventListener('DOMContentLoaded', () => {
-
-  const gifFolder = "/assets/media/!misc/gifs/";
-  const gifFiles = [
-    "bigbangblastermaster.gif",
-    "burningredblackbeamshot.gif",
-    "burningunicorn.gif",
-    "godbuster.gif",
-    "gundammasterflame.gif",
-    "gundammaxtermegablasterblue.gif",
-    "masterflammablewing.gif",
-    "roseflamerulesaber.gif",
-    "roseflamesabermode.gif",
-    "shiningbluelightwaves.gif",
-    "shiningtwilight.gif",
-    "zetagundamforwardwalk.gif"
-  ];
-
-  const randomGif = gifFiles[Math.floor(Math.random() * gifFiles.length)];
-  const gifPath = gifFolder + randomGif;
-
-  document.getElementById("gif-left").src = gifPath;
-  document.getElementById("gif-right").src = gifPath;
-});
-
-///////////////////////////////
